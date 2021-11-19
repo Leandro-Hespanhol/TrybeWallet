17d9a428f8993ce38f9ba63208d683b82deadbcb
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-11-19 16:18:50 -0300

    req02 e req 03

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
+        <header>
+          <div data-testid="email-field">{email}</div>
+          <div>
+            Despesa Total:
+            {' '}
+            <input data-testid="total-field" value="0" disabled />
+            <span data-testid="header-currency-field"> BRL </span>
+          </div>
+        </header>
         TrybeWallet
       </div>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
