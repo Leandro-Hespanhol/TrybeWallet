8cdff361d85838dfce472749c0f5d4d6e712295b
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-23 12:19:25 -0300

    Updates

---
+++ precache-manifest.28f404a7ea8e9147b02b14e8f4d492e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325a349415c53f016ee8cbb24834e734",
+    "url": "/TrybeWallet/index.html"
+  },
+  {
+    "revision": "f1b1d7c0f35e1108ccfc",
+    "url": "/TrybeWallet/static/css/main.97c2a88d.chunk.css"
+  },
+  {
+    "revision": "b44ca1e15f043f7c6db7",
+    "url": "/TrybeWallet/static/js/2.4c934b0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrybeWallet/static/js/2.4c934b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b1d7c0f35e1108ccfc",
+    "url": "/TrybeWallet/static/js/main.0ec33228.chunk.js"
+  },
+  {
+    "revision": "7064498208332b0843ee",
+    "url": "/TrybeWallet/static/js/runtime-main.e31e136f.js"
+  }
+]);
